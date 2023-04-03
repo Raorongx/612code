020266e5b3e18cbdbc5ad48facb272ac6c7120c5
Raorongx <113060209+Raorongx@users.noreply.github.com>
2023-04-02 20:44:30 -0400

    Initial commit

---
+++ src/component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.distanceIdle}>
+    <div className={styles.group20}><img className={styles.bottoms} src="https://p26-semi-asset.byteimg.com/tos-cn-i-acvclvrq33/a43137a651ce42f7ac5ee003f45501b2.png" /><img className={styles.image4} src="https://p26-semi-asset.byteimg.com/tos-cn-i-acvclvrq33/c20a8d3b321144a29569b5be132466c9.png" /><img className={styles.rectangle97} src="https://p26-semi-asset.byteimg.com/tos-cn-i-acvclvrq33/95db242c8c1049479c1c2e126a143f35.png" />
+        <div className={styles.rectangle78}></div>
+        <div className={styles.rectangle6}></div>
+        <div className={styles.group19}><img className={styles.group11} src="https://p26-semi-asset.byteimg.com/tos-cn-i-acvclvrq33/58f3d7eac49448e5914d67597d06c3a9.png" />
+            <div className={styles.autoWrapper}>
+                <p className={styles.pressRecordingToStar}>Press&nbsp;Recording&nbsp;to&nbsp;start&nbsp;fitting!<br /></p>
+                <div className={styles.autoWrapper_f779fe0d}>
+                    <p className={styles.positionYourselfInFr}>Position&nbsp;Yourself&nbsp;In&nbsp;Front&nbsp;of&nbsp;The&nbsp;Mirror&nbsp;&<br />&nbsp;Start&nbsp;the&nbsp;Journey&nbsp;</p><img className={styles.chevronsRight} src="https://p26-semi-asset.byteimg.com/tos-cn-i-acvclvrq33/5b12aba0fa32478aac9742ba80e75b3a.png" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className={styles.group26921}>
+        <div className={styles.ellipse3}></div>
+        <p className={styles.a}>􀌟</p>
+    </div>
+    <p className={styles.a_d7823cae}>👉</p>
+</div>
+  );
+}
+
+export default Component;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Component from './component';
+import "./normalize.css";
+
+const dom = document.querySelector("#root");
+ReactDOM.render(<Component />, dom);
